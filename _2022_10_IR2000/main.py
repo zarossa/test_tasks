24b4038d1a95b9d94173030ddc895fcefa58a317
zarossa <mr.kirarik@yandex.ru>
2022-11-06 09:09:47 +0700

    prestructure of parser

--- _2022_10_IR2000/main.py
+++ _2022_10_IR2000/main.py
@@ -3,7 +3,7 @@
 модуль ftp - DONE
 
 2. Скачать необходимые файлы
-модуль download
+модуль download, data
 
 3. Собрать интересующую инфу с файлов
 модуль parser
@@ -12,6 +12,171 @@
 модуль sql
 """
 
-import os, datetime, zipfile  # импорт стандартных библиотек
-import ftp, download, parser, sql  # импорт функций
+import datetime  # импорт стандартных библиотек
+import os
+import zipfile
 
+import data  # импорт функций
+# import download
+import parser
+import sql
+import ftp
+
+"""Переменная для отслеживания прогресса загрузки из консоли
+0 - отображается сокращенная информация
+1 - отображается подробная информация
+Для работы из консоли можно включить 1, но если в консоли работы не проводятся, то включаем 0, иначе логфайл захламится 
+инфой из циклов, так как вывод инфы в файл не понимает возврат коретки (\r)"""
+console_debug = 1
+
+all_data = {}  # Массив для записи данных
+data_contracts = []  # Массив для глобального хранения ID извещений
+chank_size = 500  # Размера чанка
+
+try:
+    # Перебор регионов
+    for region in data.all_regions:
+        region_name = data.regions(region)  # Получаем название региона по его индексу
+        arch_start = 1
+
+        # Проверка полученного региона на возможные ошибки
+        if region_name == 0:
+            print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), f'Региона с кодом {region} не существует')
+            continue
+
+        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), region, region_name, 'START')
+        tmp_files = []
+
+        # Устанавливаем соединение и скачиваем архивы на локальный сервер
+        ftp_connect = ftp.ftp_connect()
+
+        if ftp_connect:
+            # Переходим в папку с текущим регионом и получаем список архивов
+            file_list_curr = ftp.ftp_cwd(ftp_connect, '/fcs_regions/' + region_name + '/contracts/currMonth')
+            file_list_prev = ftp.ftp_cwd(ftp_connect, '/fcs_regions/' + region_name + '/contracts/prevMonth')
+            file_list = ftp.ftp_cwd(ftp_connect, '/fcs_regions/' + region_name + '/contracts')
+
+            for file in file_list_curr:
+                file = 'currMonth/' + file
+
+            for file in file_list_prev:
+                if file in file_list:
+                    del file
+                else:
+                    file = 'prevMonth/' + file
+
+            file_list.remove('currMonth')
+            file_list.remove('prevMonth')
+
+            file_list += file_list_curr + file_list_prev
+            del file_list_prev
+            del file_list_curr
+
+            for file in file_list:
+                # Пропускаем все архивы, кроме 2021 года и более
+                # if '_2021' not in fileName and '_2022' not in fileName:
+                year = int(
+                    file.replace('prevMonth/', '').replace('currMonth/', '').replace('contract_', '').replace(
+                        'control99doc_', '').replace(region_name + '_', '').split('_')[0][:4])
+                if year != 2022:
+                    if console_debug == 1:
+                        print('Download archives ' + str(round(arch_start * 100 / len(file_list))) + '%', end='\r')
+
+                    arch_start = arch_start + 1
+                    continue
+
+                # Закачаваем архив во временную папку tmp
+                file_zip = ftp.ftp_download(ftp_connect, file)
+                tmp_files.append(file_zip)
+
+                if console_debug == 1:
+                    print('Download archives ' + str(round(arch_start * 100 / len(file_list))) + '%', end='\r')
+
+                arch_start = arch_start + 1
+            # Закрываем соединение
+            ftp_connect.close()
+        else:
+            print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), 'Не удалось установить связь с FTP-сервером')
+            continue
+
+        if console_debug == 1:
+            print('')  # Перенос строки после вывода процента скачаных архивов (не удалять, нужно)
+        print(f'Download complete: {len(tmp_files)} archives')
+
+        chank = 0
+        all_files = 0
+        true_files = 0
+        false_files = 0
+        error_files = 0
+
+        # Перебор архивов
+        for tmp_file in tmp_files:
+            # Пропуск нечитаемого/несуществующего файла (с этим возникали какие-то проблемы,
+            # парсер ссылался на несуществующий файл, видимо где-то были дубли)
+            try:
+                arch_start = 1
+                xml_start = 1
+                file_zip_open = zipfile.ZipFile(tmp_file, 'r')  # Открытие zip-архива
+                all_files += len(file_zip_open.namelist())
+
+                # Перебор файлов в архиве
+                for file in file_zip_open.namelist():
+                    # Отсеиваем лишние файлы
+                    if 'Procedure' in file or 'sig' in file or 'Available' in file or 'contract' not in file \
+                            or 'Cancel' in file:
+                        false_files += 1
+                        continue
+
+                    true_files += 1
+
+                    try:
+                        # Получаем содержимоее файла в байтовом виде и отправляем в парсер на обработку
+                        oneObject = parser.read_xml(file_zip_open.read(file))
+
+                        oneObjectID = list(oneObject.keys())[0]
+
+                        if oneObjectID not in data_contracts:
+                            data_contracts.append(oneObjectID)
+                            all_data.update(oneObject)
+                        elif oneObject[oneObjectID]['publishDate'] >= all_data[oneObjectID]['publishDate']:
+                            all_data[oneObjectID] = oneObject[oneObjectID]
+
+                        if console_debug == 1:
+                            print('Parsing xml ' + str(round(
+                                xml_start * (arch_start * 100 / len(tmp_files)) / len(file_zip_open.namelist()))) + '%',
+                                  end='\r')
+
+                        xml_start = xml_start + 1
+
+                        chank += 1
+
+                        if chank > chank_size - 1:
+                            sql.parse_sql(all_data, region)
+                            all_data = {}
+                            chank = 0
+                    except Exception as er:
+                        error_files = error_files + 1
+                        continue
+            except Exception as ex:
+                print('Archive error')
+                continue
+
+        if console_debug == 1:
+            print('')  # Перенос строки после вывода процента скачаных архивов (не удалять, нужно)
+
+        # Добавляем оставшиеся записи в БД после обработки региона
+        sql.parse_sql(all_data, region)
+        all_data = {}
+        data_contracts = []
+        chank = 0
+
+        # Получаем список файлов временной папки и удаляем их
+        for file in os.listdir('/home/parser/contracts/tmp'):
+            os.remove('/home/parser/contracts/tmp/' + file)
+
+        print(f'All: {all_files} status: {all_files == true_files + false_files} True: {true_files} '
+              f'False: {false_files}')
+        print(f'Errors: {error_files} files')
+        print(f'{region} {region_name} STOP')
+except Exception as e:
+    print(f'!!! Ошибка в выполнении скрипта !!!\n{e}')
---
+++ _2022_10_IR2000/parser.py
@@ -0,0 +1,144 @@
+import xml.etree.ElementTree as ET
+import re
+
+
+def etree_to_json(etree_data):
+    """Функция преобразования объекта etree в JSON-массив
+    :param etree_data: etree-объект
+    :return: JSON-массив"""
+    json_data = [{etree_data.tag: parse_json(etree_data)}]
+    return json_data
+
+
+def parse_json(parents):
+    """Функция перебора структуры для JSON
+    :param parents: Родительский элемент, который перебераем
+    :return: Возвращает результат перебора"""
+    result = []
+    for child in parents:
+        if child.text is None:  # Если дочерний элемент не текст, то рекурсивно заходим глубже
+            result.append({child.tag: parse_json(child)})
+        else:  # Если дочерний элемент является текстом, то записываем результат перебора
+            result.append({child.tag: child.text})
+    return result
+
+
+def etree_to_dict(etree_data):
+    """Функция преобразования объекта etree в словарь
+    :param etree_data: etree-объект
+    :return: Возвращает словарь"""
+    dict_data = {etree_data.tag: parse_dict(etree_data)}
+    return dict_data[list(dict_data.keys())[0]]
+
+
+def parse_dict(parents):
+    """Функция перебора структуры для словаря
+    :param parents: Родительский элемент, который перебераем
+    :return: Возвращает результат перебора"""
+    result = {}  # Переменная для записи результата перебора
+
+    for child in parents:
+        if child.tag not in result:  # Если в переменной нет записи по текущему дочернему элементу, то делаем проверку
+            if child.text is None:  # Если дочерний элемент является текстом, то записываем результат перебора
+                result[child.tag] = parse_dict(child)
+            else:  # Если дочерний элемент не текст, то рекурсивно заходим глубже
+                result[child.tag] = child.text
+        else:  # Если в переменной есть запись по текущему дочернему элементу, то обрабатываем эту запись
+            if isinstance(result[child.tag], dict):  # Если текущий дочерний элемент - словарь, то создаем список
+                # и добавляем в него дочерний элемент и вызываем рекурсию
+                result[child.tag] = [result[child.tag]]
+                result[child.tag].append(parse_dict(child))
+            elif isinstance(result[child.tag], str):
+                pass
+            else:  # Если текущий дочерний элемент не словарь, то добавляем в него дочерний элемент и вызываем рекурсию
+                result[child.tag].append(parse_dict(child))
+    return result
+
+
+def parse_xml(xml_object):
+    """Функция парсера
+    :param xml_object: Объект для обработки
+    :return: Возвращает результат перебора"""
+    try:
+        result = {}  # Переменная для записи результата перебора
+        file_id = xml_object['regNum']  # Получаем id файла и создаем массив для него
+        result[file_id] = {}
+
+        # Перебор элементов объекта и запись в массив
+        for child in xml_object:
+            if isinstance(xml_object[child], str):  # Если дочерний элемент - строка, то делаем запись без обработки
+                result[file_id][child] = xml_object[child]
+            else:  # Если дочерний элемент - это не строка, то делаем обработку перед записью
+                res = get_field(xml_object[child], child)  # Вызываем рекурсивную функцию для перебора дочерних элементов
+                result[file_id].update(res)  # Результат добавляем в основной массив
+        return result
+    except Exception:
+        pass
+
+
+def get_field(parent_element, parent_name='', parent_type=False):
+    """Функция получения поля и его значения
+    :param parent_element: Родительский элемент, чьи поля будем сохранять
+    :param parent_name: Имя родителя для склейки имен (по-умолчанию пустое)
+    :param parent_type:
+    :return:
+    """
+    result = {}  # Переменная для записи результата перебора
+
+    # Перебираем родительский элемент суммируя имя элемента с дочерними именами
+    for elem in parent_element:
+        if parent_type:
+            if isinstance(parent_element[elem], str):
+                result[parent_name + elem] = parent_element[elem]
+            elif isinstance(parent_element[elem], list):
+                # ЭТО ПОЛНАЯ ЗАЛЕПА, ЭТУ ЧАСТЬ КОДА Я ПИСАЛ ПОД ЖЕСТКИМИ ПЫТКАМИ #
+                for el in parent_element[elem]:
+                    res = get_field(el, '', True)
+
+                    for r in res:
+                        e_key = parent_name + elem + r
+                        if e_key not in result:
+                            result[e_key] = res[r]
+                        else:
+                            result[e_key] += ';:;' + res[r]
+            else:
+                res = get_field(parent_element[elem], elem, True)
+                for r in res:
+                    result[r] = res[r]
+        elif isinstance(parent_element[elem], str):
+            # Если текущий элемент - это строка, то делаем запись в переменную
+            result[parent_name + elem] = parent_element[elem]
+        elif isinstance(parent_element[elem], list):
+            for el in parent_element[elem]:
+                res = get_field(el, '', True)
+
+                for r in res:
+                    e_key = parent_name + elem + r
+                    if e_key not in result:
+                        result[e_key] = res[r]
+                    else:
+                        result[e_key] += ';:;' + res[r]
+        else:
+            # Если текущий элемент - это не строка и не список, то вызываем рекурсию, передавая в нее дочерний элемент
+            res = get_field(parent_element[elem], parent_name + elem)
+            for r in res:
+                result[r] = res[r]  # Результат рекурсии записываем в переменную
+
+    return result
+
+
+def read_xml(xml_file):
+    """Функция чтения файла и первичной обработки перед парсером
+    :param xml_file: xml-файл в байтовом виде
+    :return: Возвращает информацию для БД"""
+    xml_file = xml_file.decode('utf-8')  # Преобразуем байтовый файл в строку, меняя кодировку в utf-8
+    xml_file = re.sub(r'<ns\d:', '<', xml_file)  # Убираем не нужные префиксы из тегов
+    xml_file = re.sub(r'</ns\d:', '</', xml_file)  # ...
+    xml_file = re.sub(r' xmlns:ns\d+=\"[\w:/.]+\"', '', xml_file)  # Убираем ненужные атрибуты из тегов
+    xml_file = re.sub(r' xmlns=\"[\w:/.]+\"', '', xml_file)  # ...
+    xml_file = re.sub(r'\n', '', xml_file)  # Удаляем лишние переносы строк и табуляцию
+    xml_file = re.sub(r'>[ ]+<', '><', xml_file)  # Удаляем бробелы между тегами
+    xml_file = re.sub(r'<cryptoSigns>[\w/+"=>< -]+<\/cryptoSigns>', '', xml_file)
+    xml_object = ET.fromstring(xml_file)  # Преобразуем строку в объект
+    xml_object = etree_to_dict(xml_object[0])  # Преобразуем обьект в словарь для более удобной работы
+    return parse_xml(xml_object)
